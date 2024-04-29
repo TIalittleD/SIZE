f8252e007337f26a7a5632913c98e0c6f9921e91
TIalittleD <134606972+TIalittleD@users.noreply.github.com>
2024-04-29 19:21:52 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, send_file
 from PIL import Image
 import numpy as np
@@ -39,7 +40,7 @@ def cv2ImgAddText(img, text, left, top, textColor=(255, 0, 0), textSize=20):
     # 创建一个可以在给定图像上绘图的对象
     draw = ImageDraw.Draw(img)
     # 字体的格式
-    fontStyle = ImageFont.truetype("Font\行者笔记简.ttf", textSize, encoding="utf-8")
+    fontStyle = ImageFont.truetype("Font/行者笔记简.ttf", textSize, encoding="utf-8")
     # 绘制文本
     draw.text((left, top), text, textColor, font=fontStyle)
     # 转换回OpenCV格式
@@ -89,9 +90,9 @@ def main(path):
             # 将结果写到原图上
             img_o = cv2ImgAddText(img_o, str(result), answer_box[2], answer_box[1], color, textSize)
     # 将写满结果的原图保存
-    f_name = path[:path.index(".")] + "_Result" + path[path.index("."):]
-    cv2.imwrite(f_name, img_o)
-    return f_name
+    result_image_path = os.path.join(os.path.dirname(path), os.path.basename(path).split('.')[0] + '_Result.png')
+    cv2.imwrite(result_image_path, img_o)
+    return result_image_path
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
@@ -104,10 +105,10 @@ def process_image():
         file.save(img_path)
         
         # 调用处理函数处理图像
-        result_image = main(img_path)
+        result_image_path = main(img_path)
         
         # 返回处理后的图像文件
-        return send_file(result_image, mimetype='image/png')
+        return send_file(result_image_path, mimetype='image/png')
     except Exception as e:
         return jsonify({'code': 500, 'msg': str(e)})
 
