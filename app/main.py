84786f2d378687fe2b35be692661d408d2f6fc86
TIalittleD <134606972+TIalittleD@users.noreply.github.com>
2024-04-30 12:45:56 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -118,5 +118,9 @@ def process_image():
         print('Error processing image:', str(e))
         return jsonify({'code': 500, 'msg': str(e)})
 
+@app.route('/')
+def hello_world():
+    return '欢迎使用微信云托管！'
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
