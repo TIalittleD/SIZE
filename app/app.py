14e01d80493f6bff560892dcbecd4d1e34c1f6ab
TIalittleD <134606972+TIalittleD@users.noreply.github.com>
2024-05-06 13:44:16 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -93,5 +93,5 @@ def process_image():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
 
